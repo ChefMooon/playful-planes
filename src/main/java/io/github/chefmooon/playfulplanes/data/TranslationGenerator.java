9df67d3eb05316941085f6a725a5301ae918340f
ChefMooon <139642672+ChefMooon@users.noreply.github.com>
2025-08-08 14:22:35 -0400

    add basic datagen

---
+++ src/main/java/io/github/chefmooon/playfulplanes/data/TranslationGenerator.java
@@ -0,0 +1,24 @@
+package io.github.chefmooon.playfulplanes.data;
+
+import io.github.chefmooon.playfulplanes.PlayfulPlanes;
+import io.github.chefmooon.playfulplanes.common.registry.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class TranslationGenerator extends FabricLanguageProvider {
+	protected TranslationGenerator(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
+		super(dataOutput, "en_us", registryLookup);
+	}
+
+	@Override
+	public void generateTranslations(RegistryWrapper.WrapperLookup wrapperLookup, TranslationBuilder translationBuilder) {
+		String MOD_ID = PlayfulPlanes.MOD_ID;
+
+		translationBuilder.add(ModItems.WHITE_PAPER_PLANE, "White Paper Plane");
+
+		translationBuilder.add("death.attack.playful_planes.paper_plane", "%1$s died from a Paper Plane");
+	}
+}
